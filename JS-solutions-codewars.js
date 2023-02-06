98c2b365343805f9c39026c328cb71ca02631e97
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-02-06 22:33:43 +0100

    solution of kata 8 kyu

--- JS-solutions-codewars.js
+++ JS-solutions-codewars.js
@@ -42,3 +42,12 @@ function mango(quantity, price){
     return Math.ceil(quantity / 3 * 2) * price
 }
 //-----------------------------------------------------------------------------------------------
+// Create a function called _if which takes 3 arguments:
+// a value bool and 2 functions (which do not take any parameters): func1 and func2
+//
+// When bool is truthy, func1 should be called, otherwise call the func2.
+
+function _if(bool, func1, func2) {
+    bool ? func1() : func2()
+}
+//-----------------------------------------------------------------------------------------------
